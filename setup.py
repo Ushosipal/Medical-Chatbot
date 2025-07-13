b83c92c4dd6de74cda48343a17511d9e0dc581c1
Ushosi Pal <ushosi2001@gmail.com>
2025-07-13 22:01:03 +0530

    folder structure is added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Ushosi Pal',
+    author_email= 'ushosi2001@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
